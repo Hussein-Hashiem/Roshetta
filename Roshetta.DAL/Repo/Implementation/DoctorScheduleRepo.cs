ed309654273789337d2dd3f8aeeb9b32b066f438
unknown <ibrahimali20171@hotmail.com>
2026-04-18 19:02:40 +0200

    DoctorSchedule, MedicalRecords Repos are done!

---
+++ Roshetta.DAL/Repo/Implementation/DoctorScheduleRepo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roshetta.DAL.Repo.Implementation
+{
+    public class DoctorScheduleRepo : IDoctorScheduleRepo
+    {
+        private readonly ApplicationDbContext _context;
+        public DoctorScheduleRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<DoctorSchedule> GetAll()
+        {
+            return _context.DoctorSchedules.Where(d => !d.IsDeleted).AsNoTracking();
+        }
+
+        public IQueryable<DoctorSchedule> GetById(int id)
+        {
+            return _context.DoctorSchedules.Where(d => d.Id == id).AsNoTracking();
+        }
+
+        public async Task UpdateAsync(DoctorSchedule doctorSchedule, CancellationToken cancellationToken)
+        {
+            await _context.DoctorSchedules.Where(d => d.Id == doctorSchedule.Id)
+                .ExecuteUpdateAsync(setter => setter
+                .SetProperty(p => p.StartTime, doctorSchedule.StartTime)
+                .SetProperty(p => p.EndTime, doctorSchedule.EndTime)
+                .SetProperty(p => p.Date, doctorSchedule.Date)
+                .SetProperty(p => p.MaxVisit, doctorSchedule.MaxVisit));
+        }
+    }
+}
---
+++ Roshetta.DAL/Repo/Implementation/MedicalRecordRepo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roshetta.DAL.Repo.Implementation
+{
+    public class MedicalRecordRepo : IMedicalRecordRepo
+    {
+        private readonly ApplicationDbContext _context;
+        public MedicalRecordRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddSync(MedicalRecord medicalRecord, CancellationToken cancellationToken)
+        {
+            await _context.MedicalRecords.AddAsync(medicalRecord, cancellationToken);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteSync(int id, CancellationToken cancellationToken)
+        {
+            await _context.MedicalRecords.Where(m => m.Id == id).ExecuteUpdateAsync(setter => setter.SetProperty(r => r.IsDeleted, true));
+        }
+
+        public IQueryable<MedicalRecord> GetAll()
+        {
+            return _context.MedicalRecords.Where(m => !m.IsDeleted).AsNoTracking();
+        }
+
+        public IQueryable<MedicalRecord> GetById(int id)
+        {
+            return _context.MedicalRecords.Where(m => m.Id == id && !m.IsDeleted).AsNoTracking();
+        }
+
+        public Task UpdateSync(MedicalRecord medicalRecord, CancellationToken cancellationToken)
+        {
+            return 
+        }
+    }
+}
