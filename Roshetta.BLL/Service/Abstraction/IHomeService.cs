83484ca93df2316734ddd1e190c0464b7b29e65b
Hussein-Hashiem <husshashiem@gmail.com>
2026-04-20 15:17:51 +0200

    update patient service

---
+++ Roshetta.BLL/Service/Abstraction/IHomeService.cs
@@ -0,0 +1,8 @@
+using Roshetta.BLL.Contract.Profile;
+
+namespace Roshetta.BLL.Service.Abstraction
+{
+    public interface IHomeService
+    {
+    }
+}
--- Roshetta.BLL/Service/Abstraction/IPatientService.cs
+++ Roshetta.BLL/Service/Abstraction/IPatientService.cs
@@ -1,12 +1,13 @@
-﻿using Roshetta.BLL.Contract.DoctorSchedule;
-using Roshetta.BLL.Contract.Patient;
+﻿using Roshetta.BLL.Contract;
+using Roshetta.BLL.Contract.DoctorSchedule;
+using Roshetta.BLL.Contract.Profile;
 namespace Roshetta.BLL.Service.Abstraction
 {
     public interface IPatientService
     {
-        Task<Result<IEnumerable<PatientDto>>> GetAll(CancellationToken cancellationToken = default);
-        Task<Result<PatientDto>> GetByIdAsync(string UserId, CancellationToken cancellationToken = default);
+        Task<Result<IEnumerable<ProfileResponseDto>>> GetAll(CancellationToken cancellationToken = default);
+        Task<Result<ProfileResponseDto>> GetProfileAsync(string userId, CancellationToken cancellationToken);
         Task<Result> DeleteAsync(string UserId, CancellationToken cancellationToken = default);
-        Task<Result> UpdateAsync(string userId, PatientDto patient, CancellationToken cancellationToken = default);
+        Task<Result> UpdateAsync(string userId, UpdatePatientRequestDto request, CancellationToken cancellationToken = default);
     }
 }
