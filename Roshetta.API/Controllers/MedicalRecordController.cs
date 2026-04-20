e2bb6c8b477a6badf2dafa3b0e8ceea3a412a544
unknown <ibrahimali20171@hotmail.com>
2026-04-20 14:48:03 +0200

    added controller for medical records

---
+++ Roshetta.API/Controllers/MedicalRecordController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Roshetta.API.Controllers
+{
+    public class MedicalRecordController : Controller
+    {
+        private readonly IMedicalRecordService _medicalRecordService;
+
+        public MedicalRecordController(IMedicalRecordService medicalrecordserivce)
+        {
+            _medicalRecordService = medicalrecordserivce;
+        }
+    }
+}
